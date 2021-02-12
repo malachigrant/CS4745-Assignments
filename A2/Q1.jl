3b057dd84931600927f673b1be74d15887abd75c
Malachi Grant <malachimgrant@gmail.com>
2021-02-11 20:47:44 -0400

    Finished code for A2/Q1, and added Mach2 instructions for myself.

--- A2/Q1.jl
+++ A2/Q1.jl
@@ -1,14 +1,15 @@
 using BenchmarkTools
+using Base.Threads
 
 function reduction_threads!(a)
   for k = Int(log(2, length(a)))-1:-1:0
     j = 2^k;
-    for i = 1:j
+    @threads for i = 1:j
       @inbounds a[i] = a[i] + a[i+j]
     end
   end
 end
 
-a = [1, 2, 3, 4, 5, 6, 7, 8];
+a = rand(Float64, 2^20);
 @btime reduction_threads!(a);
-println(a);
+# println(a);
---
+++ Mach2.md
@@ -0,0 +1,15 @@
+# Connecting
+
+`ssh mgrant@mach2.ace-net.ca`
+
+## Clusters
+
+Two clusters, gpu8 and gpu16. Check load with `ssh <name> uptime`.
+Connect with `ssh <name>`
+
+# Copying Files
+
+`sftp mgrant@mach2.ace-net.ca`
+
+`ls`, `cd`, `get` from remote file system.
+`lls`, `lcd`, `put` to remote file system.
